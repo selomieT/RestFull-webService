e4445a2828e12330b70925328f9fd12f3143c440
selomieT <selomie.mteshome@gmail.com>
2019-09-24 11:20:34 -0600

    Restfull-webservice project added

---
+++ src/main/java/com/st/restfullwebservice/RestfullWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.st.restfullwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullWebserviceApplication.class, args);
+    }
+
+}
